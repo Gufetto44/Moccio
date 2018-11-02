1c814e3a57ed2043990bdddc308ba466ec087250
Cosimo <cmanzione01@gmail.com>
2018-11-02 21:37:05 +0100

    Commit

---
+++ Assets/Door.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	public bool IsOpen;
+
+	Vector3 _defaultRotation;
+	Vector3 _currentRotation;
+
+	void Start () {
+		
+		// Manteniamo la rotazione iniziale della porta chiusa.
+		_defaultRotation = transform.localEulerAngles;
+		_currentRotation = _defaultRotation;
+	}
+	
+
+
+	void Update ()
+	{
+		if (IsOpen)
+			_currentRotation.z = Mathf.MoveTowards(_currentRotation.z, _defaultRotation.z+90f, Time.deltaTime * 150f);
+		else
+			_currentRotation.z = Mathf.MoveTowards(_currentRotation.z, _defaultRotation.z, Time.deltaTime * 150f);
+
+		transform.localEulerAngles = _currentRotation;
+	}
+
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		// Quando qualcosa entra all'interno del trigger della porta, controlliamo se ha lo script di un giocatore attaccato
+		// quindi cambiamo lo stato della porta di conseguenza.
+		Player player = other.gameObject.GetComponent<Player>();
+
+		if (player != null)
+			IsOpen = true;
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		Player player = other.gameObject.GetComponent<Player>();
+
+		if (player != null)
+			IsOpen = false;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float MovementSpeed = 1f;
+	public float Health = 5f;
+		
+
+	void Start () {
+		
+	}
+	
+
+	void FixedUpdate ()
+	{
+		// Ad ogni update controlliamo l'input e lo passiamo alla funzione Move
+		Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+	}
+
+
+	// Muove questo oggetto nella direzione indicata
+	void Move(Vector2 direction)
+	{
+		// transform.position contiene la posizione dell'oggetto a cui è attaccato questo script
+		// lo spostiamo nella direzione passata come parametro, moltiplicata per la velocità.
+		transform.position += (Vector3)(direction * MovementSpeed * Time.deltaTime);
+	}
+}
